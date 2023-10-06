a0d65f916ad23d1ce663938163ed7757a25858d6
Kellmer25 <79133666+Kellmer25@users.noreply.github.com>
2023-10-06 13:24:35 +0200

    Updated helpfuns

--- helpfuns.R
+++ helpfuns.R
@@ -27,3 +27,63 @@ Get_Eikon <- function(filename,Date = c("2023-08-25")){
   
   return(Data)
 }
+
+get_lobster_data = function(ticker) {
+  ticker = toupper(ticker)
+  message = read.csv(paste0("Message/", ticker, "_2012-06-21_34200000_57600000_message_1.csv")) %>%
+    as.data.frame() %>%
+    magrittr::set_colnames(c("time", "type", "order_id", "size", "price", "direction"))%>%
+    dplyr::mutate(
+      ms = round(time - 34200, 2), .after=1
+    ) %>% dplyr::mutate(
+      time = as.POSIXct(time, origin="2012-10-06 00:00:00"),
+      price = round(price / 10000, 2)
+    )
+ 
+  orderbook = read.csv(paste0("Orderbook/", ticker, "_2012-06-21_34200000_57600000_orderbook_1.csv"), header=FALSE) %>%
+    as.data.frame() %>%
+    magrittr::set_colnames(c("ask", "ask_size", "bid", "bid_size")) %>%
+    dplyr::mutate(
+      ask = round(ask / 10000, 2),
+      bid = round(bid / 10000, 2)
+    ) %>% 
+      dplyr::filter(row_number() <= n()-1)
+  
+  message = message %>%
+    dplyr::mutate(bid = orderbook$bid, .before = price) %>%
+    dplyr::mutate(ask = orderbook$ask, .after = price)
+  
+ return(message) 
+}
+
+get_polygon_time_series = function(ticker, multiplier="1", interval="minute", to_date="2023-09-11", from_date="2023-09-11") {
+  ticker = toupper(ticker)
+  api_key = paste0(
+    "https://api.polygon.io/v2/aggs/ticker/", ticker, 
+    "/range/", multiplier,
+    "/", interval,
+    "/", from_date,
+    "/", to_date,
+    "?adjusted=true&sort=asc&limit=50000&apiKey=yeGNbrt0UkaKA_Qmopz_gxbCy1m2_sDD"
+  )
+  
+  stock_df = jsonlite::fromJSON(api_key)$results %>%
+    dplyr::mutate(timestamp = lubridate::as_datetime(t / 1000)) %>%
+    dplyr::select(timestamp, c) %>%
+    dplyr::rename(price = c) %>% 
+    dplyr::arrange(timestamp)
+  
+  start = stock_df[1, 1] %>% as.POSIXct()
+  end = stock_df[nrow(stock_df), 1] %>% as.POSIXct()
+  
+  na_df = seq(from=start, to=end, by="min") %>%
+    as.data.frame() %>%
+    magrittr::set_colnames(c("timestamp"))
+  
+  stock_df = merge(stock_df, na_df, by="timestamp", all=TRUE)[1:2]
+  return(stock_df)
+}
+
+get_polygon_df = function() {
+  
+}
--- polygon_data.R
+++ polygon_data.R
@@ -68,24 +68,3 @@ to_date = "2023-09-11"
 
 ### Main
 df = data_fetcher(tickers, multiplier, interval, from_date, to_date)
-
-
-
-
-###
-library(httr)
-
-login = list(
-  "user" = "hej",
-  "password" = "1234"
-)
-
-token = "eyJhbGciOiJFUzI1NiIsIng1dCI6IkRFNDc0QUQ1Q0NGRUFFRTlDRThCRDQ3ODlFRTZDOTEyRjVCM0UzOTQifQ.eyJvYWEiOiI3Nzc3NSIsImlzcyI6Im9hIiwiYWlkIjoiMTA5IiwidWlkIjoiNnJnbVR4NG5BMklBZFh4S3xKTXFYZz09IiwiY2lkIjoiNnJnbVR4NG5BMklBZFh4S3xKTXFYZz09IiwiaXNhIjoiRmFsc2UiLCJ0aWQiOiIyMDAyIiwic2lkIjoiZDIwYWY5YzU4NzJjNGJiM2JiNzNjZjI2NWZiYjUwNjgiLCJkZ2kiOiI4NCIsImV4cCI6IjE3MDQwMDc1MjkiLCJvYWwiOiIxRiIsImlpZCI6ImFiMTk2NTQ2MGE2MjQ0YzVkNjY3MDhkYmMxNzJmMTI4In0.x3Xnm760rLW7gDKIVnhPLjq0K1b96KT1p1T74RXv3wyByXD2pn9w0UX62Js3PggIBBjZ60ddKl2HXw2SyFfE5g"
-
-res = GET("https://api.open-notify.org/astros.json")
-
-res = GET("https://gateway.saxobank.com/sim/openapi/ref/v1/instruments?ExchangeId=NYSE&Keywords=Coca Cola&AssetTypes=Stock")
-res = GET("https://gateway.saxobank.com/sim/openapi/chart/v1/charts?AssetType=FxSpot&Horizon=1&Uic=21")
-
-test = rawToChar(res$content)
-
