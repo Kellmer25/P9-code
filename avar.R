5764aa99bf497c467c07b40820e256bf9d3223c2
RasmouseJ <rasmusj.1998@gmail.com>
2023-10-16 10:21:28 +0200

    Update avar.R

--- avar.R
+++ avar.R
@@ -9,11 +9,14 @@ suppressMessages({
 })
 
 ### functions -----------------------------------------------------------------
-g_fun <- function(x) {
+g_fun <- function(x,param = list()) {
   min(x,1-x)
 }
 
-gi_fun <- function(x,a,b) {
+gi_fun <- function(x,param) {
+  a <- param$a
+  b <- param$b
+  
   x^a*(1-x)^b
 }
 
@@ -66,15 +69,83 @@ V_statistic <- function(chi, Y) {
   return(v1 - 0.5*v2)
 }
 
-psi1 <- function(kn, gi = T) {
+psi1 <- function(kn, ...) {
+  param <- unlist(list(...), recursive = F)
   
+  fun_to_call <- 'g_fun'
+  if ('a' %in% names(param)) {
+    fun_to_call <- 'gi_fun'
+  }
   
+  v1 <- sapply(X = (1:kn)/kn,FUN = fun_to_call, param)
+  v2 <- sapply(X = (0:(kn-1))/kn,FUN = fun_to_call, param)
+  return(kn*sum((v1-v2)^2))
 }
 
-A_matrix <- function() {
+psi2 <- function(kn, ...) {
+  param <- unlist(list(...), recursive = F)
   
+  fun_to_call <- 'g_fun'
+  if ('a' %in% names(param)) {
+    fun_to_call <- 'gi_fun'
+  }
+  
+  v1 <- sapply(X = (1:(kn-1))/kn,FUN = fun_to_call, param)
+  return((1/kn)*sum(v1^2))
 }
 
+invert_matrix3x3 <- function(a) {
+  if (is.matrix(a) != T) stop("The input is not a matrix")
+  if (ncol(a) != nrow(a)) stop("The matrix is not a square matrix")
+  if (ncol(a)!=3 | nrow(a)!=3) stop("The matrix is not a 3x3 matrix")
+  b <- matrix(rep(0,9), ncol=3)
+  det1a <- (a[1,3]*a[2,2]*a[3,1] - a[1,2]*a[2,3]*a[3,1] - a[1,3]*a[2,1]*a[3,2] + a[1,1]*a[2,3]*a[3,2] + a[1,2]*a[2,1]*a[3,3] - a[1,1]*a[2,2]*a[3,3])
+  det2a <- -det1a
+  if (det1a == 0) stop("The matrix is singular and not invertible")
+  numb11 <- (a[2,3]*a[3,2] - a[2,2]*a[3,3])
+  numb21 <- (a[2,1]*a[3,3] - a[2,3]*a[3,1])
+  numb31 <- (a[2,1]*a[3,2] - a[2,2]*a[3,1])
+  b[1,1] <- numb11 / det1a
+  b[2,1] <- numb21 / det1a
+  b[3,1] <- numb31 / det2a
+  numb12 <- (a[1,3]*a[3,2] - a[1,2]*a[3,3])
+  numb22 <- (a[1,3]*a[3,1] - a[1,1]*a[3,3])
+  numb32 <- (a[1,1]*a[3,2] - a[1,2]*a[3,1])
+  b[1,2] <- numb12 / det2a
+  b[2,2] <- numb22 / det1a
+  b[3,2] <- numb32 / det1a
+  numb13 <- (a[1,3]*a[2,2] - a[1,2]*a[2,3])
+  numb23 <- (a[1,1]*a[2,3] - a[1,3]*a[2,1])
+  numb33 <- (a[1,1]*a[2,2] - a[1,2]*a[2,1])
+  b[1,3] <- numb13 / det1a
+  b[2,3] <- numb23 / det1a
+  b[3,3] <- numb33 / det2a
+  return(b)
+}
+
+A_matrix <- function(kn, theta) {
+  g_param1 <- list('a' = 0.1, 'b' = 0.5)
+  g_param2 <- list('a' = 0.2, 'b' = 0.4)
+  g_param3 <- list('a' = 0.7, 'b' = 0.6)
+  
+  a_B <- function(kn, param, theta){
+    return(theta^2*psi2(kn, param)^2)
+  }
+  a_M <- function(kn, param, theta){
+    return(psi1(kn,param)*psi2(kn, param))
+  }
+  a_N <- function(kn, param, theta){
+    return(psi1(kn, param)^2/theta^2)
+  }
+  
+  A <- c(
+    a_B(kn,g_param1,theta), a_M(kn,g_param1,theta), a_N(kn,g_param1,theta),
+    a_B(kn,g_param2,theta), a_M(kn,g_param2,theta), a_N(kn,g_param2,theta),
+    a_B(kn,g_param3,theta), a_M(kn,g_param3,theta), a_N(kn,g_param3,theta)
+  ) %>% matrix(nrow = 3, ncol = 3, byrow = T)
+  
+  return(A)
+}
 
 ### Testing -------------------------------------------------------------------
 Y <- test$XwN[,c(1,2)]
@@ -82,3 +153,5 @@ Ybar <- Y_bar(Y)
 
 chi <- chi_fun(Ybar)
 V <- V_statistic(chi, Y)
+list('x' = 0.5,'a' = 0.3, 'b' = 0.7) %>% 
+  do.call(gi_fun, .)
