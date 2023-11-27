c5d4072f3f2601c2dad0543cc58df6eacccb3480
RasmouseJ <rasmusj.1998@gmail.com>
2023-11-27 14:49:59 +0100

    Create VaR_application.R

---
+++ VaR_application.R
@@ -0,0 +1,32 @@
+### Preliminaries -------------------------------------------------------------
+suppressMessages({
+  library(dplyr)
+  library(lubridate)
+  library(magrittr)
+  library(tidyr)
+  library(tictoc)
+  library(data.table)
+  library(highfrequency)
+  library(ggplot2)
+  library(xts)
+  library(stringi)
+  library(Rsolnp)
+})
+
+### Functions -----------------------------------------------------------------
+VaR <- function(omega) {
+  sqrt(t(omega)%*%sigma%*%omega)*qnorm(alpha)
+}
+
+eqn <- function(omega) {
+  return(sum(omega))
+}
+
+res <- solnp(
+  pars = as.matrix(rep(1/13, 13),ncol = 1), 
+  fun = VaR,
+  eqfun = eqn,
+  eqB = 1
+)
+
+weights <- res$pars
