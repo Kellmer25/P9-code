528b8e4cd5b5114ed06e2c120c468785d41fbe02
RasmouseJ <rasmusj.1998@gmail.com>
2023-11-30 13:57:13 +0100

    Update

--- VaR_application.R
+++ VaR_application.R
@@ -14,17 +14,22 @@ suppressMessages({
 })
 
 ### Functions -----------------------------------------------------------------
-VaR <- function(omega) {
-  sqrt(t(omega)%*%sigma%*%omega)*qnorm(alpha)
+VaR <- function(omega, sigma, alpha) {
+  res <- sqrt(t(omega)%*%sigma%*%omega)*qnorm(alpha)
+  return(res)
 }
 
-eqn <- function(omega) {
+eqn <- function(omega, sigma, alpha) {
   return(sum(omega))
 }
 
 res <- solnp(
   pars = as.matrix(rep(1/13, 13),ncol = 1), 
   fun = VaR,
+  sigma = H_end,
+  alpha = 0.95,
+  LB = rep(-100, 13),#all unknowns are restricted to be positiv
+  UB = rep(100, 13),
   eqfun = eqn,
-  eqB = 1
+  eqB = c(1,0)
 )
